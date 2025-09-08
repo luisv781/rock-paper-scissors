f9468a0cfd724ad05a76e8b7945fd2d64bb73474
Luis <luis.villela021@gmail.com>
2025-09-07 21:42:26 -0500

    display

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
+#include <Adafruit_SSD1306.h>
 #include <Arduino.h>
 #include <pins.h>
+
+#define SCREEN_ADDRESS 0x3C
+
+Adafruit_SSD1306 display(128, 32);
 
 void setup() {
     pinMode(ROCK1, INPUT);
@@ -8,11 +13,41 @@ void setup() {
     pinMode(ROCK2, INPUT);
     pinMode(PAPER2, INPUT);
     pinMode(SCISSOR2, INPUT);
+
+    display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
+    display.display();
+    delay(2000);
+    display.clearDisplay();
+
+    display.drawLine(0, 16, 127, 16, WHITE);
+    display.display();
+    delay(1000);
 }
 
 void loop() {
-    if (digitalRead(ROCK1) == LOW)
+    if (digitalRead(ROCK1) == HIGH)
     {
         digitalWrite(LED_BUILTIN, HIGH);
-    } else digitalWrite(LED_BUILTIN, LOW);
+        display.setCursor(10, 16);
+        display.setTextSize(2);
+        display.setTextColor(WHITE);
+        display.println("Rock");
+        display.display();
+
+        delay(500);
+        display.startscrollright(0x00, 0x0F);
+        delay(2000);
+        display.stopscroll();
+        delay(500);
+        display.startscrollleft(0x00, 0x0F);
+        delay(2000);
+        display.stopscroll();
+        delay(500);
+    } else
+    {
+        digitalWrite(LED_BUILTIN, LOW);
+        display.clearDisplay();
+        display.display();
+    }
+    delay(200);
 }
