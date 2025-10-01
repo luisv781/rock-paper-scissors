0ec7448fccc9420dd9c5ec15b5769f3f77b17522
luis <89324870+luisv781@users.noreply.github.com>
2025-10-01 16:41:09 +0000

    Win/Lose System

--- src/main.cpp
+++ src/main.cpp
@@ -6,25 +6,8 @@
 
 Adafruit_SSD1306 display(128, 64);
 
-String moves[3] = {"Rock", "Paper", "Scissors"};
+String moves[3] = {"Paper", "Rock", "Scissors"};
 
-void setup() {
-    pinMode(ROCK1, INPUT_PULLUP);
-    pinMode(PAPER1, INPUT_PULLUP);
-    pinMode(SCISSOR1, INPUT_PULLUP);
-    pinMode(ROCK2, INPUT_PULLUP);
-    pinMode(PAPER2, INPUT_PULLUP);
-    pinMode(SCISSOR2, INPUT_PULLUP);
-
-    display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
-    display.display();
-    delay(2000);
-
-    display.setTextSize(2);
-    display.setTextColor(WHITE);
-    display.clearDisplay();
-    display.display();
-}
 
 void scrollScreen() {
     delay(500);
@@ -114,8 +97,8 @@ void intro() {
 
 uint8_t getPlayerChosenMove(const bool player) {
     for (int move = 0; move < 3; move++) {
-        constexpr uint8_t player1Moves[] = {ROCK1, PAPER1, SCISSOR1};
-        constexpr uint8_t player2Moves[] = {ROCK2, PAPER2, SCISSOR2};
+        constexpr uint8_t player1Moves[] = {PAPER1, ROCK1, SCISSOR1};
+        constexpr uint8_t player2Moves[] = {PAPER2, ROCK2, SCISSOR2};
         if (digitalRead((!player) ? player1Moves[move] : player2Moves[move]) != HIGH)
             return move + 1;
     }
@@ -123,45 +106,102 @@ uint8_t getPlayerChosenMove(const bool player) {
     return 0;
 }
 
-void playerMove(const bool player) {
+uint8_t playerMove(const bool player) {
     while (!getPlayerChosenMove(player)) {
         delay(20);
     }
 
     uint8_t playerChosenMove = getPlayerChosenMove(player);
 
-    if (playerChosenMove != 0)
+    if (playerChosenMove)
     {
-        display.clearDisplay();
-        display.display();
+        // display.clearDisplay();
+        // display.display();
 
-        display.setCursor(10, 16);
-        display.println(!player ? "Player 1:" : "Player 2:");
+        // display.setCursor(10, 16);
+        // display.println(!player ? "Player 1:" : "Player 2:");
 
-        display.setCursor(10, 40);
-        display.println(moves[playerChosenMove - 1]);
-        display.display();
+        // display.setCursor(10, 40);
+        // display.println(moves[playerChosenMove - 1]);
+        // display.display();
 
-        scrollScreen();
+        // scrollScreen();
+        return playerChosenMove;
     }
 
     display.clearDisplay();
     display.display();
+
+    return 0;
+}
+
+void setup() {
+    pinMode(ROCK1, INPUT_PULLUP);
+    pinMode(PAPER1, INPUT_PULLUP);
+    pinMode(SCISSOR1, INPUT_PULLUP);
+    pinMode(ROCK2, INPUT_PULLUP);
+    pinMode(PAPER2, INPUT_PULLUP);
+    pinMode(SCISSOR2, INPUT_PULLUP);
+
+    display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
+    display.display();
+    delay(2000);
+
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.clearDisplay();
+    display.display();
+
+    intro();
+    delay(500);
 }
 
 void loop() {
-    intro();
-    delay(500);
+    uint8_t player1Move = 0;
+    uint8_t player2Move = 0;
 
-    bool player = false;
-    while (true) {
+    display.clearDisplay();
+    display.setCursor(0, 16);
+    display.println("Player 1's");
+    display.setCursor(30, 40);
+    display.println("Turn");
+    display.display();
+
+    player1Move = playerMove(false);
+
+    display.setCursor(0, 16);
+    display.println("Player 2's");
+    display.setCursor(30, 40);
+    display.println("Turn");
+    display.display();
+
+    player2Move = playerMove(false);
+
+
+    display.setCursor(0, 16);
+    switch (player2Move - player1Move)
+    {
+    case 1:
+    case -2:
         display.setCursor(0, 16);
-        display.println(!player ? "Player 1's" : "Player 2's");
+        display.println("Player 1");
         display.setCursor(30, 40);
-        display.println("Turn");
-        display.display();
+        display.println("Wins");
+        break;
 
-        playerMove(player);
-        player = !player;
+    case -1:
+    case 2:
+        display.setCursor(0, 16);
+        display.println("Player 2");
+        display.setCursor(30, 40);
+        display.println("Wins");
+        break;
+    
+    default:
+        display.setCursor(32, 28);
+        display.println("Tie");
+        break;
     }
+    display.display();
+    delay(3000);
 }
