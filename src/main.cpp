11c0876e1335201aebf12d9b6439e2e54b205086
Luis <luis.villela021@gmail.com>
2025-09-30 20:29:11 -0500

    game intro

--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,84 @@ void scrollScreen() {
     delay(500);
 }
 
+bool pressAnyButton() {
+    for (int i = 0; i < 6; i++) {
+        constexpr uint8_t buttons[] = {ROCK1, PAPER1, SCISSOR1, ROCK2, PAPER2, SCISSOR2};
+        if (digitalRead(buttons[i]) != HIGH)
+            return true;
+    }
+    return false;
+}
+
+void intro() {
+    display.setCursor(5, 16);
+    display.println("Rock");
+    display.display();
+    delay(1000);
+    display.setCursor(5, 32);
+    display.println("Paper");
+    display.display();
+    delay(1000);
+    display.setCursor(5, 48);
+    display.println("Scissors");
+    display.display();
+    delay(1000);
+
+    while (!pressAnyButton()) {
+        for (int i = 0; i < 5; ++i) {
+            display.fillRect(0, 0, 128, 30, BLACK);
+            display.display();
+            delay(200);
+
+            display.fillRect(0, 0, 128, 46, BLACK);
+            display.display();
+            delay(200);
+
+            display.fillRect(0, 0, 128, 64, BLACK);
+            display.setCursor(5, 16);
+            display.println("Rock");
+            display.display();
+            delay(200);
+
+            display.setCursor(5, 32);
+            display.println("Paper");
+            display.display();
+            delay(200);
+
+            display.setCursor(5, 48);
+            display.println("Scissors");
+            display.display();
+            delay(200);
+
+            if (pressAnyButton()) break;
+        }
+
+        display.clearDisplay();
+        display.display();
+        delay(200);
+
+        for (int i = 0; i < 3; ++i) {
+            display.setCursor(5, 16);
+            display.println("Press");
+            display.setCursor(5, 32);
+            display.println("Any");
+            display.setCursor(5, 48);
+            display.println("Button");
+            display.display();
+            delay(200);
+
+            display.clearDisplay();
+            display.display();
+            delay(200);
+
+            if (pressAnyButton()) break;
+        }
+    }
+
+    display.clearDisplay();
+    display.display();
+}
+
 uint8_t getPlayerMove(const bool player) {
     for (int move = 0; move < 3; move++) {
         constexpr uint8_t player1Moves[] = {ROCK1, PAPER1, SCISSOR1};
@@ -45,7 +123,7 @@ uint8_t getPlayerMove(const bool player) {
     return 0;
 }
 
-void loop() {
+void playerMove() {
     uint8_t player1Move = getPlayerMove(false);
     uint8_t player2Move = getPlayerMove(true);
 
@@ -72,4 +150,12 @@ void loop() {
 
     display.clearDisplay();
     display.display();
+}
+
+void loop() {
+    intro();
+    delay(500);
+    while (true) {
+        playerMove();
+    }
 }
